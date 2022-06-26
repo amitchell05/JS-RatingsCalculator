136785dd2b863cbb18e1a16a8f2f0d63b3b0248a
Angela Mitchell <anglarenmit@gmail.com>
2022-06-25 23:33:09 -0500

    add whitespace in change event listener callback

--- js/ratings.js
+++ js/ratings.js
@@ -26,5 +26,6 @@ let collect_ratings = () => {
 
 document.addEventListener('change', () => {
   const ratings = collect_ratings();
+
   document.querySelector('#average').value = ratings.average.toFixed(2);
 });
